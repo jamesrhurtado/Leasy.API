c7a0612684c1345d9d3f9602a1599d5a7501bed8
jrenzzo <u202118568@upc.edu.pe>
2022-10-22 00:00:18 -0500

    feat(users): Added DTO for Incoming and Outgoing resources

---
+++ Users/Domain/Services/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using Leasy.API.Users.Domain.Models;
+using Leasy.API.Users.Domain.Services.Communication;
+
+namespace Leasy.API.Users.Domain.Services;
+
+public interface IUserService
+{
+    Task<IEnumerable<User>> ListAsync();
+    Task<User> GetByIdAsync(int id);
+    Task DeleteAsync(int id);
+    Task<UserResponse> FindById(int id);
+}
