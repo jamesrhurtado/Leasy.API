2c02b973dd1a1abfa50ac331bfbde21a07fe6942
jrenzzo <u202118568@upc.edu.pe>
2022-10-22 13:30:46 -0500

    feat(users): Added Incoming and Outgoing object-mapping profiles

---
+++ Users/Mapping/ResourceToModelProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Leasy.API.Users.Domain.Models;
+using Leasy.API.Users.Resources;
+
+namespace Leasy.API.Users.Mapping;
+
+public class ResourceToModelProfile: Profile
+{
+    public ResourceToModelProfile()
+    {
+        CreateMap<SaveUserResource, User>();
+    }
+}
