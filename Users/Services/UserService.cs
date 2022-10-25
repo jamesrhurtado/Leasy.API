f83526a3570a77c5ecbc71a1b604ad6a6731f47a
jrenzzo <u202118568@upc.edu.pe>
2022-10-25 18:02:56 -0500

    feat(auth-controller): Implemented Authentication method in User Domain Services

--- Users/Services/UserService.cs
+++ Users/Services/UserService.cs
@@ -1,9 +1,13 @@
-﻿using Leasy.API.Security.Domain.Services.Communication;
+﻿using AutoMapper;
+using Leasy.API.Security.Authorization.Handlers.Interfaces;
+using Leasy.API.Security.Domain.Services.Communication;
+using Leasy.API.Security.Exceptions;
 using Leasy.API.Shared.Domain.Repositories;
 using Leasy.API.Users.Domain.Models;
 using Leasy.API.Users.Domain.Repositories;
 using Leasy.API.Users.Domain.Services;
 using Leasy.API.Users.Domain.Services.Communication;
+using BCryptNet = BCrypt.Net.BCrypt;
 
 namespace Leasy.API.Users.Services;
 
@@ -11,16 +15,41 @@ public class UserService: IUserService
 {
     private readonly IUserRepository _userRepository;
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+    private readonly IJwtHandler _jwtHandler;
 
-    public UserService(IUserRepository userRepository, IUnitOfWork unitOfWork)
+    public UserService(IUserRepository userRepository, IUnitOfWork unitOfWork, IMapper mapper, IJwtHandler jwtHandler)
     {
         _userRepository = userRepository;
         _unitOfWork = unitOfWork;
+        _mapper = mapper;
+        _jwtHandler = jwtHandler;
     }
 
     public async Task<AuthenticateResponse> Authenticate(AuthenticateRequest request)
     {
-        throw new NotImplementedException();
+        var user = await _userRepository.FindByEmailAsync(request.Email);
+        Console.WriteLine($"Request: {request.Email}, {request.Password}");
+        Console.WriteLine($"User: {user.Id}, {user.Name}, {user.LastName}, {user.Email}, {user.PasswordHash}");
+        
+        // Perform validation
+        if (user == null || !BCryptNet.Verify(request.Password, user.PasswordHash))
+        {
+            Console.WriteLine("Authentication Error");
+            throw new AppException("Username or password is incorrect");
+        }
+        
+        Console.WriteLine("Authentication successful. About to generate token");
+        
+        // Authentication is successful
+        var response = _mapper.Map<AuthenticateResponse>(user);
+        
+        // Token is generated
+        Console.WriteLine($"Response: {response.Id}, {response.Email}, {response.LastName}, {response.Name}");
+        response.Token = _jwtHandler.GenerateToken(user);
+        
+        Console.WriteLine($"Generated token is {response.Token}");
+        return response;
     }
 
     public async Task<IEnumerable<User>> ListAsync()
@@ -56,7 +85,7 @@ public class UserService: IUserService
         }
         catch (Exception e)
         {
-            //throw new AppException($"An error occurred while deleting the user: {e.Message}");
+            throw new AppException($"An error occurred while deleting the user: {e.Message}");
         }
     }
 
