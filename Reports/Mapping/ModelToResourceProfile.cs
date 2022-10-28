e6958fd974fece9263011efdbfef5e29ba75b591
jrenzzo <u202118568@upc.edu.pe>
2022-10-27 19:14:34 -0500

    feat(reports): Implemented Incoming and Outgoing Object Mapping Profiles.

---
+++ Reports/Mapping/ModelToResourceProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Leasy.API.Reports.Domain.Models;
+using Leasy.API.Reports.Resources;
+
+namespace Leasy.API.Reports.Mapping;
+
+public class ModelToResourceProfile: Profile
+{
+    public ModelToResourceProfile()
+    {
+        CreateMap<Report, ReportResource>();
+    }
+    
+}
